56c5ba64dec8ac772528deb9bcdf215958c344b6
brunofaria1322 <45635455+brunofaria1322@users.noreply.github.com>
2019-04-17 00:54:19 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -1,7 +1,46 @@
 #include <stdio.h>
-
-
+#include <stdlib.h>
+#include "func.c"
 
 int main() {
+    int choice,exit=0;
+    User user;
+    do{
+        scanf("%d",&choice);
+        switch (choice){
+            case (0):
+                exit=1;
+                break;
+            case (1):
+                user =Register();
+                break;
+            case (2):
+                break;
+            case (3):
+                break;
+            case (4):
+                break;
+            case (5):
+                break;
+            case (6):
+                break;
+            default:
+                break;
+
+        }
+    }while (!exit);
     return 0;
+}
+
+User Register() {
+    User user;
+    printf("\nNome: ");
+    scanf("%s",user.name);
+    printf("\nMorada: ");
+    scanf("%s",user.address);
+    printf("\nData de Nascimento (DD-MM-AAAA): ");
+    scanf("%s",user.date_of_birth);
+    printf("\nTelefone: ");
+    scanf("%s",user.phone_number);
+    return user;
 }

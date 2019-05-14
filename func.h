89033631d48e0e4e2b384429a34baf6a4ee9d579
brunofaria1322 <45635455+brunofaria1322@users.noreply.github.com>
2019-05-14 14:39:00 +0100

    Add files via upload

--- func.h
+++ func.h
@@ -1,7 +1,33 @@
 /*Structs*/
+typedef struct Pdi{
+    char *name, *info;
+    struct Pdi *next;
+}PDI;
+
+typedef struct Cities{
+    char *name;
+    PDI pdi;
+    struct Cities *next;
+}CITIES;
+
+
+typedef struct City{
+    char *name;
+    struct City *next;
+}CITY;
+
 typedef struct {
+    char *hot;
+    PDI pdi;
+    CITY cities;
+}UserInfo;
+
+typedef struct USER{
     char *name, *address, *date_of_birth, *phone_number;
+    UserInfo info;
+    struct USER *next;
 }User;
+
 
 /*Header - Funçoes*/
 User EditUser(User user);

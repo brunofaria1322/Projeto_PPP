a6e8aedd5ee85ca6c381b43986683afb480f279b
yourbaebea <49524805+yourbaebea@users.noreply.github.com>
2019-05-23 00:08:46 +0100

    Add files via upload
    CITIES Cidades=NULL; //lista vazia
    
    //ciclos 
        CITIES cid=NULL; //nodulo
        
        //fgets ....
        
    	cid->next=NULL;
        Cidades=insert_list(cidades,cid);

---
+++ insert_sort.c
@@ -0,0 +1,68 @@
+#include "func.c"
+
+int verifica(char palavra[], char palavra2[], int i, int j) {
+	if (j==1){
+		if (palavra[i]=='\0') return -1;
+		if (palavra2[i]=='\0') return 1;
+		if (palavra[i] > palavra2[i]) return 1;
+		if (palavra[i] == palavra2[i]) {
+			
+			i++;
+			verifica(palavra, palavra2, i);
+		}
+		
+		return -1;
+	}}
+
+PDI insert_list(PDI l,PDI node) {
+	PDI tmp,ant;
+	
+	if (l==NULL)
+		l=node;
+	else
+	if (verifica(l->name,node->name)>0) { // node< 1º elemento
+		node->next=l;
+		l=node;
+	}
+	if (verifica(l->name,node->name)<0){
+		//node no meio ou final
+		ant=l;
+		tmp=l->next;
+		while ((tmp!=NULL) && (verifica(tmp->name,node->name)<0)) {
+			ant=tmp;
+			tmp=tmp->next;
+		}
+		
+		node->next=tmp;
+		ant->next=node;
+	}
+	
+	return(l);
+}
+
+
+CITIES insert_list(CITIES l,CITIES node) {
+	PDI tmp,ant;
+	
+	if (l==NULL)
+		l=node;
+	else
+	if (verifica(l->name,node->name)>0) { // node< 1º elemento
+		node->next=l;
+		l=node;
+	}
+	if (verifica(l->name,node->name)<0){
+		//node no meio ou final
+		ant=l;
+		tmp=l->next;
+		while ((tmp!=NULL) && (verifica(tmp->name,node->name)<0)) {
+			ant=tmp;
+			tmp=tmp->next;
+		}
+		
+		node->next=tmp;
+		ant->next=node;
+	}
+	
+	return(l);
+}
